e353fb916ed31e182abde35b5f39b3a8dc65989b
khkwon01 <khkwon01@gmail.com>
2023-04-04 18:01:05 +0900

    Update check_workload.sql

--- check_workload.sql
+++ check_workload.sql
@@ -1,7 +1,12 @@
 
 -- check what type of workload your database is performing.
-
 SELECT SUM(count_read) `tot reads`, CONCAT(ROUND((SUM(count_read)/SUM(count_star))*100, 2),"%") `reads`, SUM(count_write) `tot writes`, 
        CONCAT(ROUND((SUM(count_write)/sum(count_star))*100, 2),"%") `writes` 
 FROM performance_schema.table_io_waits_summary_by_table 
 WHERE count_star > 0 ;
+
+SELECT object_schema,  CONCAT(ROUND((SUM(count_read)/SUM(count_star))*100, 2),"%") `reads`, 
+       CONCAT(ROUND((SUM(count_write)/SUM(count_star))*100, 2),"%") `writes`  
+FROM performance_schema.table_io_waits_summary_by_table  
+WHERE count_star > 0 
+GROUP BY object_schema;
