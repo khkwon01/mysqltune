00c9a6f85c14ea8619d93e0c0b2c6a228d7f8630
khkwon01 <khkwon01@gmail.com>
2023-04-04 16:50:14 +0900

    Update find_no_pk.sql

--- find_no_pk.sql
+++ find_no_pk.sql
@@ -4,3 +4,7 @@ SELECT i.TABLE_ID, t.NAME
 FROM INFORMATION_SCHEMA.INNODB_INDEXES i  
    JOIN INFORMATION_SCHEMA.INNODB_TABLES t ON (i.TABLE_ID = t.TABLE_ID)  
 WHERE      i.NAME='GEN_CLUST_INDEX';
+
+
+-- after 8.0.30 version, it provided system generated pk if you setup the below parameter..
+SET @@sql_generate_invisible_primary_key=ON;
