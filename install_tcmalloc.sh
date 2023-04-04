8b622ab7dbfbf45ee278fc9fa4aca8f8e872ad00
khkwon01 <khkwon01@gmail.com>
2023-04-04 17:56:46 +0900

    Create install_tcmalloc.sh

---
+++ install_tcmalloc.sh
@@ -0,0 +1,10 @@
+yum -y install gperftools-libs
+
+
+-- you register the following parameter in systemd
+
+systemctl edit mysqld
+===========================
+
+[Service]
+Environment="LD_PRELOAD=/usr/lib64/libtcmalloc_minimal.so.4"
