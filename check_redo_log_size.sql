ae85a0403343ed415d7d5fa55dd930290118b294
khkwon01 <khkwon01@gmail.com>
2023-04-04 17:42:02 +0900

    Create check_redo_log_size.sql

---
+++ check_redo_log_size.sql
@@ -0,0 +1,9 @@
+
+-- check redo log size generated when the service peak.
+
+SELECT VARIABLE_VALUE from performance_schema.global_status  WHERE VARIABLE_NAME='Innodb_redo_log_current_lsn' INTO @a;SELECT sleep(60)  INTO @garb ;SELECT VARIABLE_VALUE FROM performance_schema.global_status  WHERE VARIABLE_NAME='Innodb_redo_log_current_lsn' INTO @b;select  format_bytes(abs(@a - @b)) per_min, format_bytes(abs(@a - @b)*60) per_hour;
+
+
+-- if you only use a dedicated server for MySQL, you can setup the below parameter for auto control of redo and buffer, flush.
+(adjust innodb_buffer_pool_size, innodb_redo_log_capacity, innodb_flush_method automatically)
+set @@innodb_dedicated_server=ON
