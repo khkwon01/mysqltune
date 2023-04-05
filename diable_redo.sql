006ba79a8e0c69989e24621b3a6b33730c3f6e04
khkwon01 <khkwon01@gmail.com>
2023-04-05 18:30:28 +0900

    Update diable_redo.sql

--- diable_redo.sql
+++ diable_redo.sql
@@ -1,6 +1,6 @@
 -- It need to be restart MySQL when you setup the below options
 
-MySQL > ALTER INSTANCE DISABLE INNODB REDO_LOG; 
-MySQL > set global innodb_extend_and_initialize=OFF; 
-MySQL > set global innodb_max_dirty_pages_pct=10; 
-MySQL > set global innodb_max_dirty_pages_pct_lwm=10;
+ALTER INSTANCE DISABLE INNODB REDO_LOG; 
+set global innodb_extend_and_initialize=OFF; 
+set global innodb_max_dirty_pages_pct=10; 
+set global innodb_max_dirty_pages_pct_lwm=10;
