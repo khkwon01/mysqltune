4378201f0138b713fe05bc77c5be82c13a41c009
khkwon01 <khkwon01@gmail.com>
2024-04-30 10:22:57 +0900

    Update install_jemalloc.sh

--- install_jemalloc.sh
+++ install_jemalloc.sh
@@ -1,1 +1,20 @@
+## install enterprise linux repository
+yum install epel-release
 
+## install jemalloc library
+yum install jemalloc
+
+## check the library whether it is installed or not
+ls -ltr /usr/lib64/libjemalloc.so.2
+
+## edit mysqld service file using systemctl and add environment variable
+systemctl edit mysqld
+
+[Service]
+Environment="LD_PRELOAD=/usr/lib64/libjemalloc.so.2"
+
+## reload the system daemon
+systemctl daemon-reload
+
+## restart MySQL
+systemctl restart mysql
