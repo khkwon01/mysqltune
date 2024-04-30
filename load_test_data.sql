38387fe81189c46f5b03352266159d4c048b0d47
khkwon01 <khkwon01@gmail.com>
2024-04-30 13:49:26 +0900

    Create load_test_data.sql

---
+++ load_test_data.sql
@@ -0,0 +1,36 @@
+CREATE TABLE `students` (
+  `first_name` varchar(32) NOT NULL,
+  `last_name` varchar(32) NOT NULL,
+  `mobile_no` varchar(10) DEFAULT NULL,
+  `age` int DEFAULT NULL,
+  KEY `age_index` (`age`)
+) --ENGINE=MEMORY DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DELIMITER $$
+
+CREATE PROCEDURE load_data()
+begin
+  DECLARE i int DEFAULT 0;
+  while i < 100000 do
+    INSERT INTO `students`
+              (
+                          `first_name`,
+                          `last_name`,
+                          `age`,
+                          `mobile_no`
+              )
+              VALUES
+              (
+                          conv(floor(rand() * 99999999999999), 10, 36),
+                          conv(floor(rand() * 99999999999999), 10, 36),
+                          round(rand()*100,2),
+                          lpad(floor(rand() * 10000000000), 10, '0')
+              );
+
+    set i = i + 1;
+  end while;
+end
+$$
+
+DELIMITER ;
