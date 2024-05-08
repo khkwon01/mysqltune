6337a6bcb8e760d9d041a8e99d2dc7a9b55e34d6
khkwon01 <khkwon01@gmail.com>
2024-05-08 13:49:28 +0900

    Update disable_redo.sql

--- disable_redo.sql
+++ disable_redo.sql
@@ -1,4 +1,5 @@
 -- It need to be restart MySQL when you setup the below options
+-- 데이터에 대한 빠른 load가 필요할 경우 disable후 작업후 enable (binlog도 disable하면 효과적)
 
 ALTER INSTANCE DISABLE INNODB REDO_LOG; 
 set global innodb_extend_and_initialize=OFF; 
