1e034fd16ea1902e9fc0f0b3289661ce06fddd2e
khkwon01 <khkwon01@gmail.com>
2024-05-19 23:19:47 +0900

    Create check_global_error_count.sh

---
+++ check_global_error_count.sh
@@ -0,0 +1,6 @@
+SELECT
+  SUM(SUM_ERROR_RAISED) AS global_errors
+FROM
+  performance_schema.events_errors_summary_global_by_error
+WHERE
+  ERROR_NUMBER NOT IN (1287);   -- exclude deprecated warning
