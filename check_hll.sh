35bbcd6121b224a904f575ac6cff894fab596e97
khkwon01 <khkwon01@gmail.com>
2024-05-20 17:32:34 +0900

    Create check_hll.sh

---
+++ check_hll.sh
@@ -0,0 +1,6 @@
+# setup related metric for monitoring HLL
+# HLL means that Innodb is keeping a significant number of old row versions regarding long-running and abandoned transaction has not committed.
+set global innodb_monitor_enable=module_trx;
+
+# check for HLL using the following query (if it is less than 1000, it's normal, otherwise, if it greater than 100,000, check the reason)
+SELECT * FROM INFORMATION_SCHEMA.INNODB_METRICS where name like 'trx_rseg_his%' ORDER BY NAME;
