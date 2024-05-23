fe27efcf0f4c4428881ba978e79b71af4c6b2b72
khkwon01 <khkwon01@gmail.com>
2024-05-23 15:20:30 +0900

    Update check_long_transaction.sh

--- check_long_transaction.sh
+++ check_long_transaction.sh
@@ -17,3 +17,22 @@ FROM
 WHERE
       trx.state = 'ACTIVE'
   AND trx.timer_wait > 1000000000000 * 1\G  -- 1 second(unit :pico)
+
+
+SELECT
+  trx.thread_id AS thread_id,
+  MAX(trx.event_id) AS trx_event_id,
+  MAX(ROUND(trx.timer_wait/1000000000000,3)) AS trx_runtime,
+  SUM(ROUND(stm.timer_wait/1000000000000,3)) AS exec_time,
+  SUM(stm.rows_examined) AS rows_examined,
+  SUM(stm.rows_affected) AS rows_affected,
+  SUM(stm.rows_sent) AS rows_sent
+FROM
+       performance_schema.events_transactions_current trx
+  JOIN performance_schema.events_statements_history   stm
+    ON stm.thread_id = trx.thread_id AND stm.nesting_event_id = trx.event_id
+WHERE
+      stm.event_name LIKE 'statement/sql/%'
+  AND trx.state = 'ACTIVE'
+  AND trx.timer_wait > 1000000000000 * 1
+GROUP BY trx.thread_id\G
