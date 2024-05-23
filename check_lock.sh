8c31c8b20a11dfa9f881beb8972da51922748f84
khkwon01 <khkwon01@gmail.com>
2024-05-23 15:53:53 +0900

    Update check_lock.sh

--- check_lock.sh
+++ check_lock.sh
@@ -1,3 +1,5 @@
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+
 SELECT index_name, lock_type, lock_mode, lock_status, lock_data
 FROM   performance_schema.data_locks
 WHERE  object_name = '<<change>>';
